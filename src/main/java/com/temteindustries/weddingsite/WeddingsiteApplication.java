66af97e4eb27bfaab6353f177485c01167632cd5
jtemte3 <jtemte3@gmail.com>
2019-02-27 19:32:48 -0600

    initial commit

---
+++ src/main/java/com/temteindustries/weddingsite/WeddingsiteApplication.java
@@ -0,0 +1,13 @@
+package com.temteindustries.weddingsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeddingsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeddingsiteApplication.class, args);
+	}
+
+}
