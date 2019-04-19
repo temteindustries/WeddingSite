fba13c61745dcd483d157be6dac352d4b59d7f4c
jtemte3 <jtemte3@gmail.com>
2019-04-18 21:10:26 -0500

    added error handling

--- src/main/java/com/temteindustries/weddingsite/Controller/controller.java
+++ src/main/java/com/temteindustries/weddingsite/Controller/controller.java
@@ -14,26 +14,47 @@ public class controller {
         return "index.html";
     }
 
+    @GetMapping("/error")
+    public String error() {
+        return "error.html";
+    }
+
     @PostMapping("/update")
     public String updateGuestList(@RequestBody String stmnt) {
+        boolean cached = false;
         GsonJsonParser parser = new GsonJsonParser();
         String json = cleanRequest(stmnt);
+        if(json.isEmpty()){
+            return"redirect:/error";
+        }
         List<Object> guestList = parser.parseList(json);
-        boolean cached = CacheService.addToCache(guestList);
+        try {
+            cached = CacheService.addToCache(guestList);
+        }catch (Exception e){
+            System.out.println("Error caching: \n" + e.getMessage());
+            return"redirect:/error";
+        }
         if (cached){
             System.out.println("RSVP cached");
             return"redirect:/";
         }
         else{
             System.out.println("Error caching");
-            return"redirect:/";
+            return"redirect:/error";
         }
     }
 
     private String cleanRequest(@RequestBody String stmnt) {
-        String json = stmnt.substring(5); // remove "json=" from stmnt
-        json = json.substring(0, json.length()-2);
-        return json;
+        String json = null;
+        try {
+            json = stmnt.substring(5); // remove "json=" from stmnt
+            json = json.substring(0, json.length() - 2);
+        }catch (Exception e){
+            System.out.println("Error cleaning request: \n" + e.getMessage());
+            return json;
+        }finally {
+            return json;
+        }
     }
 
 }
